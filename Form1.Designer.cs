411b323f8691b049cff63a1590c1679239a8f996
MoroXDev <szczepanekrafal27@gmail.com>
2026-03-04 09:35:31 +0100

    Prawie skonczone

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            wzory = new ComboBox();
+            button1 = new Button();
+            wynik = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(101, 57);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(59, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(101, 128);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(68, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(101, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 15);
+            label1.TabIndex = 2;
+            label1.Text = "bok1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(101, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 3;
+            label2.Text = "bok2";
+            // 
+            // wzory
+            // 
+            wzory.FormattingEnabled = true;
+            wzory.Items.AddRange(new object[] { "Prostokat_Obwod", "Kwadrat_Obwod", "Kolo_Obwod", "Prostokat_Pole", "Kwadrat_Pole", "Trojkat_Pole", "Kolo_Pole" });
+            wzory.Location = new Point(367, 57);
+            wzory.Name = "wzory";
+            wzory.Size = new Size(133, 23);
+            wzory.TabIndex = 4;
+            wzory.Text = "Prostokat";
+            wzory.SelectedIndexChanged += wzory_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(367, 121);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 28);
+            button1.TabIndex = 5;
+            button1.Text = "Oblicz";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // wynik
+            // 
+            wynik.AutoSize = true;
+            wynik.Location = new Point(367, 175);
+            wynik.Name = "wynik";
+            wynik.Size = new Size(41, 15);
+            wynik.TabIndex = 6;
+            wynik.Text = "wynik:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(wynik);
+            Controls.Add(button1);
+            Controls.Add(wzory);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Kalkulator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private ComboBox wzory;
+        private Button button1;
+        private Label wynik;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,131 @@ namespace Kalkulatorv2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double a))
+            {
+                if (double.TryParse(textBox2.Text, out double b))
+                {
+
+                    switch (wzory.SelectedItem)
+                    {
+                        //Pola
+                        case "Prostokat_Pole":
+                            wynik.Text = "wynik: " + Prostokat_Pole(a, b);
+
+                            break;
+                        case "Trojkat_Pole":
+                            wynik.Text = "wynik: " + Trojkat_Pole(b, a);
+                            break;
+
+                        case "Kolo_Pole":
+                            wynik.Text = "wynik: " + Kolo_Pole(a);
+                            break;
+
+                        case "Kwadrat_Pole":
+                            wynik.Text = "wynik: " + Kwadrat_Pole(a);
+                            break;
+                        //Obwody
+                        case "Prostokat_Obwod":
+                            wynik.Text = "wynik: " + Prostokat_Obwod(a, b);
+                            break;
+
+                        case "Kolo_Obwod":
+                            wynik.Text = "wynik: " + Kolo_Obwod(a);
+                            break;
+
+                        case "Kwadrat_Obwod":
+                            wynik.Text = "wynik: " + Kwadrat_Obwod(a);
+                            break;
+                        default:
+                            MessageBox.Show("Nie wybra³eœ wzoru!");
+                            break;
+
+                    }
+                    return;
+                }
+            }
+            MessageBox.Show("Nie wpisa³eœ tylko cyfr!");
+        }
+
+        double Prostokat_Pole(double a, double b)
+        {
+            return a * b;
+        }
+        double Prostokat_Obwod(double a, double b)
+        {
+            return 2 * a + 2 * b;
+        }
+
+        double Trojkat_Pole(double a, double h)
+        {
+            return (a * h) / 2;
+        }
+      
+        double Kwadrat_Pole(double a)
+        {
+            return a * a;
+        }
+        double Kwadrat_Obwod(double a)
+        {
+            return 4 * a;
+        }
+
+        double Kolo_Pole(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        double Kolo_Obwod(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        private void wzory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (wzory.SelectedItem)
+            {
+                case "Prostokat_Pole":
+                case "Prostokat_Obwod":
+                    label1.Text = "Bok1";
+                    label2.Text = "Bok2";
+                    label1.Show();
+                    label2.Show();
+                    textBox1.Show();
+                    textBox2.Show();
+
+                    break;
+                case "Trojkat_Pole":
+                case "Trojkat_Obwod":
+                    label1.Text = "Wysokoœæ";
+                    label2.Text = "Podstawa";
+                    label1.Show();
+                    label2.Show();
+                    textBox1.Show();
+                    textBox2.Show();
+                    break;
+
+                case "Kolo_Pole":
+                case "Kolo_Obwod":
+                    label1.Text = "Promieñ";
+                    label1.Show();
+                    label2.Hide();
+                    textBox1.Show();
+                    textBox2.Hide();
+                    break;
+
+                case "Kwadrat_Pole":
+                case "Kwadrat_Obwod":
+                    label1.Text = "Bok";
+                    label1.Show();
+                    label2.Hide();
+                    textBox1.Show();
+                    textBox2.Hide();
+                    break;
+
+            }
+        }
     }
 }
